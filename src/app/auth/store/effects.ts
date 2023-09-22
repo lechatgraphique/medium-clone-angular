614eeaa1f7590c6854f881923e7c3fe95d4f79bd
Bastien CLEMENT <hello@lechatgraphique.fr>
2023-09-22 19:16:29 +0200

    Persistence service

--- src/app/auth/store/effects.ts
+++ src/app/auth/store/effects.ts
@@ -5,14 +5,16 @@ import {authActions} from './actions'
 import {AuthService} from "../services/auth.service";
 import {CurrentUserInterface} from "../../shared/types/currentUser.interface";
 import {HttpErrorResponse} from "@angular/common/http";
+import {PersistenceService} from "../../shared/services/persistance.service";
 
 export const registerEffect = createEffect(
-  (actions$ = inject(Actions), authService = inject(AuthService)) => {
+  (actions$ = inject(Actions), authService = inject(AuthService), persistenceService = inject(PersistenceService)) => {
     return actions$.pipe(
       ofType(authActions.register),
       switchMap(({request}) => {
         return authService.register(request).pipe(
           map((currentUser: CurrentUserInterface) => {
+            persistenceService.set('accessToken', currentUser.token);
             return authActions.registerSuccess({currentUser})
           }),
           catchError((errorResponse: HttpErrorResponse) => {

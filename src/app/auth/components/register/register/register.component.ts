f8ae94c5bb0168d58131f0b727880cc7d1e19202
Bastien CLEMENT <hello@lechatgraphique.fr>
2023-09-22 10:23:17 +0200

    Register layout and reactive form

--- src/app/auth/components/register/register/register.component.ts
+++ src/app/auth/components/register/register/register.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {RouterLink} from "@angular/router";
+import {FormBuilder, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
   selector: 'mc-register',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink, ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
-
+  form = this.fb.nonNullable.group({
+    username: ['', Validators.required],
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+  })
+  constructor(private fb: FormBuilder) { }
+  onSubmit(): void {
+    console.log('form:', this.form.getRawValue());
+  }
 }

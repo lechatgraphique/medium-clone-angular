030af1255538276b6f2aefcbad691d1c36afeaea
Bastien CLEMENT <hello@lechatgraphique.fr>
2023-09-22 17:24:16 +0200

    Update auth reducers

--- src/app/auth/store/actions.ts
+++ src/app/auth/store/actions.ts
@@ -1,6 +1,7 @@
-import {createActionGroup, emptyProps, props} from '@ngrx/store'
+import {createActionGroup, props} from '@ngrx/store'
 import {RegisterRequestInterface} from "../types/registerRequest.interface";
 import {CurrentUserInterface} from "../../shared/types/currentUser.interface";
+import {BackendErrorsInterface} from "../../shared/types/backendErrors.interface";
 
 
 export const authActions = createActionGroup({
@@ -8,6 +9,6 @@ export const authActions = createActionGroup({
   events: {
     Register: props<{request: RegisterRequestInterface}>(),
     'Register success': props<{currentUser: CurrentUserInterface}>(),
-    'Register failure': emptyProps(),
+    'Register failure': props<{errors: BackendErrorsInterface}>(),
   },
 })
--- src/app/auth/store/reducers.ts
+++ src/app/auth/store/reducers.ts
@@ -5,13 +5,30 @@ import {AuthStateInterface} from "../types/authState.interface";
 
 const initialState: AuthStateInterface = {
   isSubmitting: false,
+  isLoading: false,
+  currentUser: undefined,
+  validationErrors: null
 }
 
 const authFeature = createFeature({
   name: 'auth',
   reducer: createReducer(
     initialState,
-    on(authActions.register, (state) => ({...state, isSubmitting: true}))
+    on(authActions.register, (state) => ({
+      ...state,
+      isSubmitting: true,
+      validationErrors: null
+    })),
+    on(authActions.registerSuccess, (state, action) => ({
+      ...state,
+      isSubmitting: false,
+      currentUser: action.currentUser
+    })),
+    on(authActions.registerFailure, (state, action) => ({
+      ...state,
+      isSubmitting: false,
+      validationErrors: action.errors
+    })),
   ),
 })
 
@@ -19,4 +36,7 @@ export const {
   name: authFeatureKey,
   reducer: authReducer,
   selectIsSubmitting,
+  selectIsLoading,
+  selectCurrentUser,
+  selectValidationErrors
 } = authFeature
